696989df091dcfb1b1715cc49a6c2824ce835961
WilberElias <ia.wilberelias@ufg.edu.sv>
2024-10-19 13:54:07 -0600

    Actualizar servicios de contacto, historial de reservas, estado de reservas, reservas, roles, salas y usuarios

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
+  private apiUrl = 'http://localhost:3000/api/reservations';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getReservations(): Observable<Reservation[]> {
+    return this.http.get<Reservation[]>(this.apiUrl);
+  }
+
+  getReservationById(id: number): Observable<Reservation> {
+    return this.http.get<Reservation>(`${this.apiUrl}/${id}`);
+  }
+
+  createReservation(reservation: Reservation): Observable<Reservation> {
+    return this.http.post<Reservation>(this.apiUrl, reservation);
+  }
+
+  updateReservation(id: number, reservation: Reservation): Observable<Reservation> {
+    return this.http.put<Reservation>(`${this.apiUrl}/${id}`, reservation);
+  }
+
+  deleteReservation(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Room } from '../models/room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
+  private apiUrl = 'http://localhost:3000/api/rooms';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getRooms(): Observable<Room[]> {
+    return this.http.get<Room[]>(this.apiUrl);
+  }
+
+  getRoomById(id: number): Observable<Room> {
+    return this.http.get<Room>(`${this.apiUrl}/${id}`);
+  }
+
+  createRoom(room: Room): Observable<Room> {
+    return this.http.post<Room>(this.apiUrl, room);
+  }
+
+  updateRoom(id: number, room: Room): Observable<Room> {
+    return this.http.put<Room>(`${this.apiUrl}/${id}`, room);
+  }
+
+  deleteRoom(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
