626e6bdf5e959930467f4c998e68cf2a13f16056
Wilber Ernesto Elias Avalos <ia.wilberelias@ufg.edu.sv>
2024-11-12 17:30:01 -0600

    Agregar componente de pie de página: implementar diseño y enlaces a redes sociales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../app/shared/footer/footer.component';
 import { NavbarComponent } from '../app/shared/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NavbarComponent],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent],
   template: `
-
     <app-navbar></app-navbar>
     <router-outlet></router-outlet> <!-- Aquí se cargarán las vistas según las rutas -->
-  
+    <app-footer></app-footer>
   `
 })
 export class AppComponent {
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { materialModules } from '../../models/material-imports';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [materialModules],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
