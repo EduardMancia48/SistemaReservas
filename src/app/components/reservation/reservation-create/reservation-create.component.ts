19fa84216f50dfb314104b452b1544cdda96d80f
WilberElias <ia.wilberelias@ufg.edu.sv>
2024-11-19 14:01:30 -0600

    Actualizar dependencias de Angular y refactorizar componente de creación de reservas: implementar inyección de dependencias, agregar validaciones de hora completa y establecer hora mínima en la siguiente hora completa.

--- src/app/components/reservation/reservation-create/reservation-create.component.ts
+++ src/app/components/reservation/reservation-create/reservation-create.component.ts
@@ -1,35 +1,61 @@
-import { Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
-import { materialModules } from '../../../models/material-imports';
 import { Reservation } from '../../../models/reservation';
+import { materialModules } from '../../../models/material-imports';
 
 @Component({
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatNativeDateModule, ...materialModules],
-    providers: [
-        provideNativeDateAdapter()
-    ],
-    selector: 'app-reservation-create',
-    templateUrl: './reservation-create.component.html',
-    styleUrls: ['./reservation-create.component.css']
+  selector: 'app-reservation-create',
+  templateUrl: './reservation-create.component.html',
+  styleUrls: ['./reservation-create.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatNativeDateModule, materialModules
+  ],
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReservationCreateComponent {
   reservationForm: FormGroup;
   minDate: Date;
+  minTime: string;
 
-  constructor(
-    public dialogRef: MatDialogRef<ReservationCreateComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    private fb: FormBuilder
-  ) {
+  dialogRef = inject(MatDialogRef<ReservationCreateComponent>);
+  data = inject(MAT_DIALOG_DATA);
+  fb = inject(FormBuilder);
+
+  constructor() {
     this.minDate = new Date(); // Establecer la fecha mínima en la fecha actual
+    this.minTime = this.getNextFullHour(); // Establecer la hora mínima en la siguiente hora completa
     this.reservationForm = this.fb.group({
       fecha_reserva: ['', Validators.required],
-      hora_inicio: ['', Validators.required],
-      hora_fin: ['', Validators.required]
+      hora_inicio: ['', [Validators.required, this.validateFullHour.bind(this)]],
+      hora_fin: ['', [Validators.required, this.validateFullHour.bind(this)]]
     });
+  }
+
+  getNextFullHour(): string {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    if (minutes > 0) {
+      hours += 1; // Avanzar a la siguiente hora completa si hay minutos en la hora actual
+    }
+
+    return `${hours.toString().padStart(2, '0')}:00`;
+  }
+
+  validateFullHour(control: any): { [key: string]: boolean } | null {
+    const value = control.value;
+    if (value) {
+      const [hours, minutes] = value.split(':').map(Number);
+      if (minutes !== 0) {
+        return { invalidTime: true };
+      }
+    }
+    return null;
   }
 
   onNoClick(): void {
@@ -51,5 +77,14 @@ export class ReservationCreateComponent {
       console.log('Reserva creada:', reservation);
       this.dialogRef.close(reservation);
     }
+  }
+
+  onHoraInicioChange(): void {
+    const horaInicio = this.reservationForm.get('hora_inicio')?.value;
+    const [hours, minutes] = horaInicio.split(':').map(Number);
+    const horaFinMin = new Date();
+    horaFinMin.setHours(hours + 1, 0); // Establecer la hora de fin a la siguiente hora completa
+    const minHoraFin = horaFinMin.toTimeString().slice(0, 5);
+    this.reservationForm.get('hora_fin')?.setValue(minHoraFin);
   }
 }
